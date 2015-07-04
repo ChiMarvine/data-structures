b9c44194a9df11af0f291c60e93ba16715dda018
ChiMarvine <chi.marvine@gmail.com>
2015-07-04 11:42:24 -0700

    hash + part of binary

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,37 @@
 var BinarySearchTree = function(value){
+	var node = {};
+	node.value = value;
+	node.left = undefined;
+	node.right = undefined;
+
+
+
+	node.insert = function(value){
+		if(value > node.value) {
+			if(node.right === undefined) {
+				node.right = BinarySearchTree(value);
+			} else {
+				node.right.insert(value);
+			}
+		} else {
+			if(node.left === undefined) {
+				node.left = BinarySearchTree(value);
+			} else {
+				node.left.insert(value);
+			}
+		}
+	}
+
+	node.contains = function(value){
+
+	}
+
+	node.depthFirstLog = function(callback){
+
+	}
+
+	return node;
+
 };
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,25 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  
+  if(this._storage.get(i) === undefined) {
+  	var bucket = {};
+  	bucket[k] = v;
+  	return this._storage.set(i, bucket);
+  }
+  if(this._storage.get(i) !== undefined) {
+  	this._storage.get(i)[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
-
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	 this._storage.get(i)[k] = null;
 };
 
 
